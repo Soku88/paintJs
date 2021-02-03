a49d654d2d204638d0e074de8b6b469e5a1a1c5d
Soku88 <essence.soku@gmail.com>
2021-02-03 17:51:06 +0900

    add mouse event

---
+++ app.js
@@ -0,0 +1,37 @@
+const canvas = document.querySelector("#jsCanvas");
+const ctx = canvas.getContext("2d");
+
+ctx.strockStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
+
+let painting = false;
+
+function stopPainting() {
+  painting = false;
+}
+
+function startPainting() {
+  painting = true;
+}
+
+function onMouseMove(e) {
+  const x = e.offsetX;
+  const y = e.offsetY;
+  if (!painting) {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+  } else {
+    ctx.lineTo(x, y);
+  }
+}
+
+function onMouseDown(e) {
+  painting = true;
+}
+
+if (canvas) {
+  canvas.addEventListener("mousemove", onMouseMove);
+  canvas.addEventListener("mousedown", startPainting);
+  canvas.addEventListener("mouseup", stopPainting)
+  canvas.addEventListener("mouseleave", stopPainting)
+}
