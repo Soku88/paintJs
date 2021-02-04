8805d1e3a97c8dd683f76b315cf72329cc90a03e
Soku88 <essence.soku@gmail.com>
2021-02-05 02:34:00 +0900

    change color

--- app.js
+++ app.js
@@ -1,37 +1,45 @@
 const canvas = document.querySelector("#jsCanvas");
 const ctx = canvas.getContext("2d");
+const color = document.getElementsByClassName("jsColor");
 
-ctx.strockStyle = "#2c2c2c";
-ctx.lineWidth = 2.5;
+canvas.width = 500;
+canvas.height = 500;
+
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 1;
 
 let painting = false;
 
 function stopPainting() {
-  painting = false;
+	painting = false;
 }
 
 function startPainting() {
-  painting = true;
+	painting = true;
 }
 
 function onMouseMove(e) {
-  const x = e.offsetX;
-  const y = e.offsetY;
-  if (!painting) {
-    ctx.beginPath();
-    ctx.moveTo(x, y);
-  } else {
-    ctx.lineTo(x, y);
-  }
+	const x = e.offsetX;
+	const y = e.offsetY;
+	if (!painting) {
+		ctx.beginPath();
+		ctx.moveTo(x, y);
+	} else {
+		ctx.lineTo(x, y);
+		ctx.stroke();
+	}
 }
-
-function onMouseDown(e) {
-  painting = true;
+function handleColor(e) {
+	const bgColor = e.target.style.backgroundColor;
+	console.log(bgColor);
+	ctx.strokeStyle = bgColor;
 }
 
 if (canvas) {
-  canvas.addEventListener("mousemove", onMouseMove);
-  canvas.addEventListener("mousedown", startPainting);
-  canvas.addEventListener("mouseup", stopPainting)
-  canvas.addEventListener("mouseleave", stopPainting)
+	canvas.addEventListener("mousemove", onMouseMove);
+	canvas.addEventListener("mousedown", startPainting);
+	canvas.addEventListener("mouseup", stopPainting);
+	canvas.addEventListener("mouseleave", stopPainting);
 }
+
+Array.from(color).forEach((color) => color.addEventListener("click", handleColor));
